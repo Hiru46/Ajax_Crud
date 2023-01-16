cfa3a07296414ba370af7e058755fa9983d1e503
ShaniIshara20 <shaniranasinghe20@gmail.com>
2023-01-16 16:08:34 +0530

    add a new line

--- database.php
+++ database.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ajax Crud by using PHP</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 </head>
 <body>
     <p>new line</p>
